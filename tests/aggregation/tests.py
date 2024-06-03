4219fd0dc7faa4e359921893c8c6ed23917399f2
Chris Muthig <camuthig@gmail.com>
2024-06-02 20:23:35 -0600

    Fix broken test

--- tests/aggregation/tests.py
+++ tests/aggregation/tests.py
@@ -1289,6 +1289,17 @@ class AggregateTestCase(TestCase):
         class MultiArgAgg(Max):
             output_field = DecimalField()
 
+            def as_sql(self, compiler, connection, **extra_context):
+                copy = self.copy()
+                # Most database backends do not support compiling multiple arguments on
+                # the Max aggregate, and that isn't what is being tested here anyway. To
+                # avoid errors, the extra argument is just dropped.
+                copy.set_source_expressions(
+                    copy.get_source_expressions()[0:1] + [None, None]
+                )
+
+                return super(MultiArgAgg, copy).as_sql(compiler, connection)
+
         with self.assertRaisesMessage(TypeError, "Complex aggregates require an alias"):
             Book.objects.aggregate(MultiArgAgg("pages", "price"))
 
